8fae23c53f806428416a10605aa36a66b20ee287
soundharyadev <30386716+soundharyadev@users.noreply.github.com>
2017-08-05 15:47:11 +0530

    Create sumofnnumbers.c

---
+++ sumofnnumbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int sum=0,n,val,i;
+printf("enter the numbers to be added:");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+printf("the sum of numbers are:");
+scanf("%d",&val)
+}
+getch();}
